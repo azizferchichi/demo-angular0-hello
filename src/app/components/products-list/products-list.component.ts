8a547feb3e5c0aeec90e54fd254868af2911fb51
Aziz FERCHICHI <mohamedaziz.ferchichi@sofrecom.com>
2018-12-11 09:49:14 +0100

    Demo

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/Domain/iproduct';
 
 @Component({
   selector: 'app-products-list',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsListComponent implements OnInit {
 
+  data: IProduct[] = [
+    {'title': 'Disque externe USB', 'unitPrice': 150, 'quantity': 2},
+    {'title': 'Souris', 'unitPrice': 10.5, 'quantity': 5},
+    {'title': 'Clavier', 'unitPrice': 650, 'quantity': 1}
+  ];
   constructor() { }
 
   ngOnInit() {
